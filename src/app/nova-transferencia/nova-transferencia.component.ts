3ed537fcc8216b931890eec8c56e4948fb05f43e
Luan Rocha Silva <luan.silva@virtus.ufcg.edu.br>
2023-04-13 10:30:24 -0300

    Final do curso

---
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -0,0 +1,42 @@
+import { Transferencia } from './../../models/transferencia.model';
+import { TransferenciaService } from './../services/transferencia.service';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-nova-transferencia',
+  templateUrl: './nova-transferencia.component.html',
+  styleUrls: ['./nova-transferencia.component.scss']
+})
+export class NovaTransferenciaComponent implements OnInit {
+
+  @Output() aoTransferir = new EventEmitter<any>();
+
+  valor: number;
+  destino: number;
+
+  constructor(private service: TransferenciaService, private router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+  transferir() {
+    console.log('Solicitada nova transferência');
+    const valorEmitir: Transferencia = {valor: this.valor, destino: this.destino};
+
+    this.service.adicionar(valorEmitir).subscribe(resultado => {
+      console.log(resultado);
+      this.limparCampos();
+
+      this.router.navigateByUrl('extrato')
+    }, (error) => {
+      console.log(error);
+    }
+    )
+  }
+
+  limparCampos() {
+    this.valor = 0;
+    this.destino = 0;
+  }
+}
